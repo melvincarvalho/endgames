d5a5cd1d9aeba2637612e79cd4f5f53b95d4f5a5
Melvin Carvalho <melvincarvalho@gmail.com>
2026-01-27 14:21:38 +0100

    Deploy to GitHub Pages

---
+++ assets/js/aba21aa0.2bd920e8.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkchess_endgames=globalThis.webpackChunkchess_endgames||[]).push([[5742],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ build/assets/js/a7456010.c6ac500a.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkchess_endgames=globalThis.webpackChunkchess_endgames||[]).push([[1235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
